8e31d63f9b28ad85c5bc0f564d844685ddad9ff4
gibanator <georgy.gibizov@gmail.com>
2022-11-11 17:18:32 +0200

    Car class: -changed accelerate method -added refuel method Main class: -added calls of methods from Car class

--- src/Car.java
+++ src/Car.java
@@ -7,8 +7,20 @@ class Car{
     }
 
     public void accelerate(){
-        System.out.println("Car is accelerating");
-        amountOfFuel--;
+        if (amountOfFuel>0) {
+            System.out.println("Car is accelerating");
+            amountOfFuel--;
+        }
+        else {
+            System.out.println("Car cannot accelerate. Amount of fuel is 0.");
+        }
+    }
+
+    public void refuel(int amount){
+        System.out.println("Fuel in the tank: " + amountOfFuel);
+        System.out.println("Amount to be filled in: " + amount);
+        amountOfFuel+=amount;
+        System.out.println("Fuel on the tank after the refuel: " + amountOfFuel);
     }
 
     public void printData(){
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ class Main{
         Car car1 = new Car();
         car1.brand = "Fiat";
         car1.model = "500";
-        car1.amountOfFuel = 50;
+        car1.amountOfFuel = 1;
         car1.printData();
         car1.brake();
         car1.accelerate();
-
+        car1.printData();
+        car1.accelerate();
+        car1.refuel(10);
+        car1.printData();
 
     }
 }
